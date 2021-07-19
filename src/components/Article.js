63ae4458e6e6769eff09831905ad37ca157297d3
Troy <troyakins99@yahoo.com>
2021-07-19 11:44:08 -0500

    commit tree 4f6ba3f98a49f719e8970ed7386abf92d444bbc7

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,8 +3,8 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* uncomment the line below to pass the test */}
-      {/* please pass this test */}
+      {/* uncomment the line below to pass the test  */}
+       please pass this test
     </div>
   );
 }
